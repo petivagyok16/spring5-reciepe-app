d6b603be0a31680bba3524579e0982347b2d51a3
Peter P <peter.palocz91@gmail.com>
2018-04-05 12:03:51 +0200

    feat(Notes): add Notes model

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,16 @@
+package guru.springframework.domain;
+
+public class Recipe {
+
+	private String description;
+	private Integer prepTime;
+	private Integer cookTime;
+	private Integer servings;
+	private String source;
+	private String url;
+	private String directions;
+	// Todo: add
+	// pirvate Difficulty difficulty;
+	private Byte[] image;
+	private Notes notes;
+}
