2736c7ca9e12a1fad0a8acbb451aaf9a249b238b
Peter P <peter.palocz91@gmail.com>
2018-04-11 12:47:39 +0200

    feat(RecipeController): add new endpoints

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,15 +2,12 @@ package guru.springframework.controllers;
 
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * Created by jt on 6/19/17.
- */
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -20,8 +17,8 @@ public class RecipeController {
 		this.recipeService = recipeService;
 	}
 
-	@RequestMapping("/recipe/show/{id}")
-	public String showById(@PathVariable String id, Model model){
+	@RequestMapping("/recipe/{id}/show")
+	public String showById(@PathVariable String id, Model model) {
 
 		model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
@@ -35,9 +32,27 @@ public class RecipeController {
 		return "recipe/recipeform";
 	}
 
+	@PostMapping
+	@RequestMapping("recipe")
 	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
 		RecipeCommand savedCommand = this.recipeService.saveRecipeCommand(command);
 
 		return "redirect:/recipe/show/" + savedCommand.getId();
+	}
+
+	@RequestMapping("recipe/{id}/update")
+	public String updateRecipe(@PathVariable String id, Model model) {
+		model.addAttribute("recipe", this.recipeService.findCommandById(Long.valueOf(id)));
+
+		return "recipe/recipeform";
+	}
+
+	@GetMapping
+	@RequestMapping("recipe/{id}/delete")
+	public String deleteById(@PathVariable String id) {
+		log.debug("Deleting id: " + id);
+
+		this.recipeService.deleteById(Long.valueOf(id));
+		return "redirect:/";
 	}
 }
